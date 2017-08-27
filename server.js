a756a314859625208cec7ea79e67db9e3a39c34e
danillosl <danillosl@gmail.com>
2017-08-27 17:31:41 -0300

    removendo entradas para views

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var home = require('./routes/home');
 var tasks = require('./routes/tasks');
 
 var port = 3000;
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(cors());
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', home);
 app.use('/api/tarefas', tasks);
+
+app.get('*', function (req, res) {
+    res.sendfile(path.join(__dirname, 'views/index.html'));
+})
 
 app.listen(port, function () {
     console.log('Server iniciado na porta ' + port);
